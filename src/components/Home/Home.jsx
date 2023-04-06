0cf29e05dd1f46986b02647a10a7d337e04689f3
Rajib-debnath1 <rajibdebnath519@gmail.com>
2023-04-06 23:55:24 +0600

    buy now button added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
+import Tshirt from '../Tshirt/Tshirt';
+import './Home.css';
 
 const Home = () => {
+    const [] = useState([]);
     const tshirts = useLoaderData();
+
+    const handleAddToCart = tshirt =>{
+        console.log(tshirt)
+    }
     return (
-        <div>
-            <h2>This is home: {tshirts.length}</h2>
+        <div className='home-container'>
+           <div className='t-shirt-container'>
+           {
+            tshirts.map(tshirt =><Tshirt
+                key={tshirt._id}
+                tshirt = {tshirt}
+                handleAddToCart = {handleAddToCart}
+            ></Tshirt>)
+           }
+           </div>
+           <div className='cart-container'>
+                <Cart></Cart>
+           </div>
         </div>
     );
 };
